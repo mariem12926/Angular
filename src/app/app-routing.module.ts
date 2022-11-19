115a4e1f708b221318b00bacb5e2f2d0b639d7bb
mariem12926 <meryem.turki@esprit.tn>
2022-11-19 15:00:03 +0100

    ex2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticlesComponent } from './articles/articles.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
 import { ProductComponent } from './product/product.component';
@@ -7,8 +8,10 @@ import { ProductComponent } from './product/product.component';
 const routes: Routes = [
   {path:'products',component:ProductComponent},
   {path:'emploi',component:OffresEmploiComponentComponent},
+  {path:'articles',component:ArticlesComponent},
   {path:'',redirectTo:'products',pathMatch:'full'},
-  {path:'**',component:NotFoundComponent}
+  {path:'**',component:NotFoundComponent},
+  {path:'',redirectTo:'articles',pathMatch:'full'}
 ];
 
 @NgModule({
--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -8,10 +8,24 @@ import { Articles } from '../core/Articles';
 })
 export class ArticlesComponent implements OnInit {
   titre!:string
+  listeArticles:Articles[]=[]
+  nombre!:number
+  countText!:string
   constructor() { }
 
   ngOnInit(): void {
     this.titre="Les articles de jour"
-      
+     this.listeArticles=[{titre:'Le championnat du monde',contenu:'Le champion du monde de cette année est .....',auteur:'Med Taher',date:'12/12/2005',categorie:'Sport'}, 
+     {titre:'Les nouveaux parents',contenu:'Les nouveaux parents. ..',auteur:'Ahmed Said',date:'11/11/2018',categorie:'Education'},
+     {titre:'Comment écrire votre CV',contenu:'Pour réussir à décraucher un emploi...',auteur:'Marie Elsa',date:'02/04/2017',categorie:'Travail'}]
+     
+  }
+  count(){
+    this.nombre=0
+    for(let i=0;i<this.listeArticles.length;i++)
+    { if(this.listeArticles[i].categorie!='Travail') this.nombre++
+    }
+
+    
   }
 }
